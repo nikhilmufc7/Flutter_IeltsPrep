9885f956022c5ccacc9bd472c275fea19726e5c6
Nikhil Singh <Singh.nikhil999@gmail.com>
2020-03-28 21:33:32 +0530

    added custom clipper for login screen

---
+++ lib/widgets/bezierContainer.dart
@@ -0,0 +1,25 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+import 'customClipper.dart';
+
+class BezierContainer extends StatelessWidget {
+  const BezierContainer({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Transform.rotate(
+      angle: -pi / 3.5,
+      child: ClipPath(
+        clipper: ClipPainter(),
+        child: Container(
+          height: MediaQuery.of(context).size.height * .5,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(color: Color(0xFF21BFBD)),
+        ),
+      ),
+    ));
+  }
+}
