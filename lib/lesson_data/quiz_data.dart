3fe7d473d13ac69dd7e637fd3bbaacc54c0a5be8
Nikhil Singh <Singh.nikhil999@gmail.com>
2020-04-11 22:24:22 +0530

    added quiz model and dummy data

---
+++ lib/lesson_data/quiz_data.dart
@@ -0,0 +1,34 @@
+import 'package:ielts/models/quiz.dart';
+
+List getQuizData() {
+  return [
+    Quiz(question: [
+      'this',
+      'another'
+    ], options: [
+      [
+        "Taco Bello",
+        "Minnesota",
+        "arkansas",
+      ],
+      ["1", "2"]
+    ], answers: [
+      'arkansas',
+      '2'
+    ]),
+    Quiz(question: [
+      'this',
+      'another'
+    ], options: [
+      [
+        "Taco Bello",
+        "Minnesota",
+        "arkansas",
+      ],
+      ["1", "2"]
+    ], answers: [
+      'arkansas',
+      '2'
+    ]),
+  ];
+}
---
+++ lib/models/quiz.dart
@@ -0,0 +1,7 @@
+class Quiz {
+  List question;
+  List<dynamic> options;
+  List answers;
+
+  Quiz({this.question, this.options, this.answers});
+}
