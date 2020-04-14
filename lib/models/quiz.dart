89331ce8a845677d831c0a40cd04d3a1555e1241
Nikhil Singh <Singh.nikhil999@gmail.com>
2020-04-15 02:37:34 +0530

    added firebase models and crud data for quizzes

--- lib/models/quiz.dart
+++ lib/models/quiz.dart
@@ -5,10 +5,28 @@ class Quiz {
   String quizTitle;
   double indicatorValue;
 
-  Quiz(
-      {this.question,
-      this.options,
-      this.answers,
-      this.quizTitle,
-      this.indicatorValue});
+  Quiz({
+    this.question,
+    this.options,
+    this.answers,
+    this.quizTitle,
+    this.indicatorValue,
+  });
+
+  Quiz.fromMap(Map<dynamic, dynamic> snapshot, String id)
+      : question = snapshot['question'] ?? '',
+        options = snapshot['options'] ?? {},
+        answers = snapshot['answers'] ?? [],
+        quizTitle = snapshot['quizTitle'] ?? '',
+        indicatorValue = snapshot['indicatorValue'] ?? '';
+
+  toJson() {
+    return {
+      "question": question,
+      "options": options,
+      "answers": answers,
+      "quizTitle": quizTitle,
+      "indicatorValue": indicatorValue,
+    };
+  }
 }
---
+++ lib/viewModels/quizCrudModel.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:ielts/locator.dart';
+import 'package:ielts/models/quiz.dart';
+import 'package:ielts/services/quizApi.dart';
+
+class QuizCrudModel extends ChangeNotifier {
+  QuizApi _api = locator<QuizApi>();
+  List<Quiz> quiz;
+
+  Future<List<Quiz>> fetchQuiz() async {
+    var result = await _api.getDataCollection();
+    quiz = result.documents
+        .map((doc) => Quiz.fromMap(doc.data, doc.documentID))
+        .toList();
+    return quiz;
+  }
+
+  Stream<QuerySnapshot> fetchQuizAsStream() {
+    return _api.streamDataCollection();
+  }
+
+  Future<Quiz> getQuizById(String id) async {
+    var doc = await _api.getDocumentById(id);
+    return Quiz.fromMap(doc.data, doc.documentID);
+  }
+
+  Future removeQuiz(String id) async {
+    await _api.removeDocument(id);
+    return;
+  }
+
+  Future updateQuiz(Quiz data, String id) async {
+    await _api.updateDocument(data.toJson(), id);
+    return;
+  }
+
+  Future addQuiz(Quiz data) async {
+    var result = await _api.addDocument(data.toJson());
+
+    return;
+  }
+}
