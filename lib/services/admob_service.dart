56605fb294b7e739115b29ce042aec598487eade
Nikhil Singh <Singh.nikhil999@gmail.com>
2020-07-16 20:29:53 +0530

    created ad mob service

---
+++ lib/services/admob_service.dart
@@ -0,0 +1,9 @@
+class AdMobService {
+  String getAdMobAppId() {
+    return 'ca-app-pub-3133734302958653~2859571909';
+  }
+
+  String getBannerAdId() {
+    return 'ca-app-pub-3940256099942544/6300978111';
+  }
+}
